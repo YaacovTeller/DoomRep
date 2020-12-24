1fc92be012b125661b362ffcbba092888fff847a
Yaacov Teller <yaacovteller@gmail.com>
2020-12-24 19:50:17 +0200

    added Shlai minigame

--- DoomSound.js
+++ DoomSound.js
@@ -1,21 +1,4 @@
 "use strict";
-//SOUNDS - Guns and creatures
-// sound-loader
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    //   this.sound.setAttribute("loop", "infinite");  For a sort of macabre doom-rap experience
-    hideElement(this.sound);
-    document.body.appendChild(this.sound);
-    this.play = function () {
-        this.sound.play();
-    };
-    this.stop = function () {
-        this.sound.pause();
-    };
-}
 var Pshot = new sound("sound/DSPISTOL.WAV");
 var SGshot = new sound("sound/DSSHOTGN.WAV");
 var SawUp = new sound("sound/DSSAWUP.WAV");
